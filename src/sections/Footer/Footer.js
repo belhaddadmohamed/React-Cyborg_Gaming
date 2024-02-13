6de662e99968615d84d6ea52a4b63152f621a7f2
belhaddadMohamed <belhaddad.mohamed7@gmail.com>
2024-02-13 20:18:28 +0100

    footer.js

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -1,13 +1,20 @@
 import './Footer.css'
 
+// import {jquery} from '../../vendor/jquery/jquery.min.js'
+// import {bootstrap} from "../../vendor/bootstrap/js/bootstrap.min.js"
+// import {isotope} from "../../assets/js/isotope.min.js"
+// import {carousel} from "../../assets/js/owl-carousel.js"
+// import {tabs} from "../../assets/js/tabs.js"
+// import {popup} from "../../assets/js/popup.js"
+// import {custom} from "../../assets/js/custom.js"
 
 const Footer = () => {
     return (
         <>
-              <footer>
-                <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
+            <footer>
+                <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
                     <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved. 
                     
                     <br/>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a></p>
@@ -15,6 +22,18 @@ const Footer = () => {
                 </div>
                 </div>
             </footer>
+
+
+            {/* Scripts */}
+            {/* Bootstrap core JavaScript */}
+            <script src="../../vendor/jquery/jquery.min.js"></script>
+            <script src="../../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+            <script src="../../assets/js/isotope.min.js"></script>
+            <script src="../../assets/js/owl-carousel.js"></script>
+            <script src="../../assets/js/tabs.js"></script>
+            <script src="../../assets/js/popup.js"></script>
+            <script src="../../assets/js/custom.js"></script>
         </>
     )
 }
